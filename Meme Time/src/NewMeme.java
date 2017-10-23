608fbb149ce0ada804e3136f314d4d097d07787c
BT_1N3_11 <BT_1N3_11@BT_1N3_11.bktech.local>
2017-10-23 13:15:23 -0400

    Testing code

--- Meme Time/src/NewMeme.java
+++ Meme Time/src/NewMeme.java
@@ -2,6 +2,15 @@
 public class NewMeme {
     public static void main(String[] args)
     {
-    	System.out.println("I hate eclipse");
+    	String[] test = {"potato","potato1","potato2"};
+    	String[] test1 = new String[1];
+    	test1[0] = changeTomato(test);
+    	System.out.println(test[2]);
+    	System.out.println(test1[0]);
     }
+	public static String changeTomato(String[] string)
+	{
+		string[2] = "tomato";
+		return string[2];
+	}
 }
