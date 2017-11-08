483a6b8b6b2ef5eb6a68c6a46afe4124a74d0ce8
BT_1N3_11 <BT_1N3_11@BT_1N3_11.bktech.local>
2017-11-08 13:16:05 -0500

    done

--- Lab 3.1/src/ArrayMethods.java
+++ Lab 3.1/src/ArrayMethods.java
@@ -1,4 +1,53 @@
+import java.lang.reflect.Array;
 
 public class ArrayMethods {
 
+	public static void main(String[] args)
+	{
+	
+	
+	}
+	public static int[ ] removeDuplicates(int[ ] org)
+	{
+		boolean [] copy = new boolean[org.length];
+		int count = 0;
+		int x = 0;
+		for(int  i = 0; i < org.length-1; i++)
+		{
+			for(int j = i+1; j < org.length; j++)
+			{
+				if(org[i] == org[j])
+				{
+					copy[j] = true;
+					count++;
+				}
+				else {
+					copy[j] = false;
+				}
+			}
+		}
+		int[] finalCopy = new int[org.length-count];
+		while(x < (org.length-count))
+		{
+			if(!copy[x])
+			{
+				finalCopy[x] = org[x];
+				x++;
+			}
+		}
+		return finalCopy;
+	}
+	
+	
+	
+	
+	public static int[] copyArray(int[] intArray)
+	{
+		int[] copy = new int[intArray.length];
+		for(int i = 0; i< intArray.length; i++)
+		{
+			copy[i] = intArray[i];
+		}
+		return copy;
+	}
 }
