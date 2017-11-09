2320f7c6158cd7fbd96b02d7017453babf5a82d1
BT_1N3_11 <BT_1N3_11@BT_1N3_11.bktech.local>
2017-11-09 13:15:51 -0500

    Fix question 1

--- Lab 3.1/src/ArrayMethods.java
+++ Lab 3.1/src/ArrayMethods.java
@@ -4,7 +4,12 @@ public class ArrayMethods {
 
 	public static void main(String[] args)
 	{
-	
+		int[] yes = {0,1,2,3,3,4,1,3,4,4,1};
+		
+		for(int i = 0; i <  removeDuplicates(yes).length-1; i++)
+		{
+			System.out.println( removeDuplicates(yes)[i]);
+		}
 	
 	}
 	public static int[ ] removeDuplicates(int[ ] org)
@@ -12,9 +17,10 @@ public class ArrayMethods {
 		boolean [] copy = new boolean[org.length];
 		int count = 0;
 		int x = 0;
-		for(int  i = 0; i < org.length-1; i++)
+		copy[0] = false;
+		for(int  i = 0; i < org.length-2; i++)
 		{
-			for(int j = i+1; j < org.length; j++)
+			for(int j = i+1; j < org.length-1; j++)
 			{
 				if(org[i] == org[j])
 				{
@@ -22,18 +28,32 @@ public class ArrayMethods {
 					count++;
 				}
 				else {
-					copy[j] = false;
+					if(!copy[j])
+					{
+						copy[j] = false;
+					}
+					continue;
 				}
+				
 			}
 		}
 		int[] finalCopy = new int[org.length-count];
-		while(x < (org.length-count))
+	/*	for(int i = 0; i <  finalCopy.length-1; i++)
+		{
+			System.out.println( finalCopy[i]);
+		}*/
+	//	System.out.println("------------------------------------------------");
+		int secondcount = 0;
+		while(x < finalCopy.length)
 		{
 			if(!copy[x])
 			{
-				finalCopy[x] = org[x];
+				finalCopy[secondcount] = org[x];
 				x++;
+				secondcount++;
 			}
+			else
+				x++;
 		}
 		return finalCopy;
 	}
