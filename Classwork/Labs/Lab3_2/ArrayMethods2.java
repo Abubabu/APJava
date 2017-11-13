aa18551919ae17c6809b5f11bfb1c2bebcd073e3
BT_1N3_11 <BT_1N3_11@BT_1N3_11.bktech.local>
2017-11-13 13:15:16 -0500

    Added lab 3.2

---
+++ Classwork/Labs/Lab3_2/ArrayMethods2.java
@@ -0,0 +1,29 @@
+package Lab3_2;
+
+public class ArrayMethods2 {
+	public static void main(String[] args)
+	{
+	
+	
+	}
+	public static int[] merge(String[] list1, String[] list2)
+	{
+		String[] result = new String[list1.length+list2.length];
+		boolean listsMerged = true;
+		int onePosition = 0;
+		int twoPosition = 0;
+		while(listsMerged)
+		{
+			if(((int)list1[onePosition]) < ((int)list2[twoPosition])))
+		}
+		return null;
+	}
+	
+	public static String[] sortMerge(String[] list) {
+		if(list.length)
+	}
+	public static int partition(int[] list)
+	{
+		return 0;
+	}
+}
