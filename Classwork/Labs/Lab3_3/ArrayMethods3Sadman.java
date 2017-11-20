6ac462d2b915e5dea06ec59872bbc5a50a812ca7
BT_1N3_11 <BT_1N3_11@BT_1N3_11.bktech.local>
2017-11-20 13:10:42 -0500

    sad

--- Classwork/Labs/Lab3_3/ArrayMethods3Sadman.java
+++ Classwork/Labs/Lab3_3/ArrayMethods3Sadman.java
@@ -3,27 +3,23 @@ package Lab3_3;
 public class ArrayMethods3Sadman {
 	public static void main(String[] args) {
 		int[] list1 = {1,3,56,2,9,27};
-		insertionSort(list1);
-	}
-	
-	public static void insertionSort(int [] list1)
-	{
-		int placeholder;
-		int exit;
-		while(true) 
-		{
-			
-		}
+		pushIntArray(list1,4);
 		for(int x : list1)
 		{
 			System.out.println(x);
 		}
 	}
-	public static void swapStringArray(int[] array, int i, int j) {
+	
+	public static void insertionSort(int [] list1)
+	{
+		
+	}
+	public static void swapIntArray(int[] array, int i, int j) {
 		int temp = array[i];
 		array[i] = array[j];
 		array[j] = temp;
 	}
+	
 	/*Insertion takes each element from the array, and adds it to the front of the
 	array in the correct order. We need to keep track of where we are inserting
 	elements as we sort.
