90ebc91194e77af0e91915623caa52cbffcaca86
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2017-12-05 13:11:23 -0500

    fix to loop properly through the hashed array

--- Classwork/Sort_A_Mania/SortCompetition/CountingSort.java
+++ Classwork/Sort_A_Mania/SortCompetition/CountingSort.java
@@ -2,34 +2,56 @@ package SortCompetition;
 
 public class CountingSort {
 	public static void main(String[] args) {
+		/*int j = 0;
+		double avg1 = 0;
+		int[] time1 = new int[10000];
+		while(j < 100)
+		{
+			for(int i = 0; i < 1; i++)
+			{
+				for(int k = 0; k < 10000; k++)
+				{
+					time1[k] = (int) (Math.random()*10000); //need to have semi-sorted array
+				}
+				long startime  = System.nanoTime();
+				countingSort(time1);
+				long endtime = System.nanoTime(); // 88534.76
+				long finalTime = endtime - startime;
+				avg1 += finalTime;
+			}
+			j++;
+	}*/
+		int[] time1 = {1,3,1,6,1,3,6,0,1,2};
+		long startime  = System.nanoTime();
+		countingSort(time1);
+		long endtime = System.nanoTime(); // 88534.76
+		long finalTime = endtime - startime;
 		
+		System.out.print(finalTime);
 	}
 		public static void countingSort(int[] array) {
 			int[] hashedArray = new int[array.length];
 			
 			for(int i = 0; i < array.length; i++) {
-				hashedArray[i] += 1;
+				hashedArray[array[i]] += 1;
 			}
 			
-			for(int j = 0; j < hashedArray.length; j++) {
+			for(int j = 1; j < hashedArray.length; j++) {
 				hashedArray[j] += hashedArray[j - 1];
 			}
-			
-			for(int j = 0; j < hashedArray.length; j++) {
-				hashedArray[j] += hashedArray[j - 1];
-			}
-			
 			int count = 0;
-			int hashCount = 0;
 			int numcount = 0;
+			int initialcount = count;
 			while(count < array.length) {
-				for(int i = 0; i < hashedArray[numcount];i++) {
-					array[count] = count;
-					hashCount++;
-					count++;
+						for(int z = hashedArray[numcount - 1]; z < hashedArray[numcount];z++) {
+							array[count] = numcount;
+							count++;
+							z++;
+							if(count >= array.length)
+								break;
+						}
+						numcount++;
 				}
-				numcount++;
-			}
 				
 		}
 }
