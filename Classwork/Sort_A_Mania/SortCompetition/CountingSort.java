5da7da77f19473df111742e4cf5c8abf5b11ac79
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2017-12-05 12:30:48 -0500

    counting

--- Classwork/Sort_A_Mania/SortCompetition/CountingSort.java
+++ Classwork/Sort_A_Mania/SortCompetition/CountingSort.java
@@ -1,5 +1,35 @@
 package SortCompetition;
 
 public class CountingSort {
-
+	public static void main(String[] args) {
+		
+	}
+		public static void countingSort(int[] array) {
+			int[] hashedArray = new int[array.length];
+			
+			for(int i = 0; i < array.length; i++) {
+				hashedArray[i] += 1;
+			}
+			
+			for(int j = 0; j < hashedArray.length; j++) {
+				hashedArray[j] += hashedArray[j - 1];
+			}
+			
+			for(int j = 0; j < hashedArray.length; j++) {
+				hashedArray[j] += hashedArray[j - 1];
+			}
+			
+			int count = 0;
+			int hashCount = 0;
+			int numcount = 0;
+			while(count < array.length) {
+				for(int i = 0; i < hashedArray[numcount];i++) {
+					array[count] = count;
+					hashCount++;
+					count++;
+				}
+				numcount++;
+			}
+				
+		}
 }
