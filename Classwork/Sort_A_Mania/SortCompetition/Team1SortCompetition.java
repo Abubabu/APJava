f1f20fa09a6cf5ba4209917e14588e9505287d1b
BT_1N3_11 <BT_1N3_11@BT_1N3_11.bktech.local>
2017-12-07 13:14:01 -0500

    y

--- Classwork/Sort_A_Mania/SortCompetition/Team1SortCompetition.java
+++ Classwork/Sort_A_Mania/SortCompetition/Team1SortCompetition.java
@@ -61,7 +61,7 @@ public class Team1SortCompetition extends SortCompetition {
 		return median(   medianFixed(	nums[	((int)nums.length/2)] ), medianFixed( nums[ ((int)nums.length/2)+1] )); 
 	}
 	public int challengeFive(Comparable[] arr, Comparable query) {
-		insertionSortCom(arr);
+		TimsortsOP(arr);
 	}
 	private static void insertionSort(int [] list1)
 	{
@@ -228,6 +228,64 @@ public class Team1SortCompetition extends SortCompetition {
 			int[] otherHalf = Arrays.copyOfRange(nums, half, nums.length);
 			return mergeIntOptimized(  TimSortsOP( oneHalf ), TimSortsOP( otherHalf));		
 		}
+	}
+	public static Comparable[] TimSortsOP(Comparable[] nums)
+	{
+		if(nums.length < 8)
+		{
+			insertionSortCom(nums);
+			return nums;
+		}
+		else
+		{
+			int half = (nums.length/2 );
+			Comparable[] oneHalf = Arrays.copyOfRange(nums, 0, half);
+			Comparable[] otherHalf = Arrays.copyOfRange(nums, half, nums.length);
+			return mergeComOptimized(  TimSortsOP( oneHalf ), TimSortsOP( otherHalf));		
+		}
+	}
+	private static Comparable[] mergeComOptimized(Comparable[] nums1, Comparable[] nums2) {
+		Comparable[] result = new Comparable[nums1.length+nums2.length];
+		int onePosition = 0;
+		int twoPosition = 0;
+		while(true)
+		{
+			if(onePosition > nums1.length-1) //when all of list1 is in the results array
+			{
+				for(int i = twoPosition; i < nums2.length; i++)
+				{
+					result[nums1.length+i] = nums2[i];
+				}
+				break;
+			}
+			if(twoPosition > nums2.length-1) //when all of list2 is in the results array
+			{
+				for(int i = onePosition; i < nums1.length; i++)
+				{
+					result[nums2.length+i] = nums1[i];
+				}
+				break;
+			}
+			if(  nums1[onePosition].compareTo(nums2[twoPosition]) < 0) // if list1 is lower
+			{
+				result[onePosition+twoPosition] = nums1[onePosition];
+				onePosition++;
+				continue;
+			}
+			else if(  nums1[onePosition].compareTo(nums2[twoPosition]) > 0) // if list2 is lower
+			{
+				result[onePosition+twoPosition] = nums2[twoPosition];
+				twoPosition++;
+				continue;
+			}
+			else
+			{
+				result[onePosition+twoPosition] = nums2[twoPosition];
+				twoPosition++;
+				continue;
+			}
+		}
+		return result;
 	}
 	private static int[] mergeIntOptimized(int[] nums1, int[] nums2) {
 		int[] result = new int[nums1.length+nums2.length];
--- Classwork/Sort_A_Mania/SortCompetition/TimSortForComparable.java
+++ Classwork/Sort_A_Mania/SortCompetition/TimSortForComparable.java
@@ -1,18 +1,20 @@
 package SortCompetition;
 
+import java.util.Arrays;
+
 public class TimSortForComparable {
 	public static void main(String[] args)
 	{
 		String[] test = {"i","z","x","a","b","l"};
 		System.out.print("yes");
-		insertionSortString(test);
+		insertionSortCom(test);
 		System.out.print("yes");
 		for(int i = 0; i < test.length; i++)
 		{
 			System.out.println(test[i]);
 		}
 	}
-	public static void insertionSortString(Comparable [] list1)
+	public static void insertionSortCom(Comparable [] list1)
 	{
 		int fhp = 1;
 		if(list1[0].compareTo(list1[1]) > 0)
@@ -50,6 +52,64 @@ public class TimSortForComparable {
 					continue;
 			}	
 		}
+	}
+	public static Comparable[] TimSortsOP(Comparable[] nums)
+	{
+		if(nums.length < 8)
+		{
+			insertionSortCom(nums);
+			return nums;
+		}
+		else
+		{
+			int half = (nums.length/2 );
+			Comparable[] oneHalf = Arrays.copyOfRange(nums, 0, half);
+			Comparable[] otherHalf = Arrays.copyOfRange(nums, half, nums.length);
+			return mergeComOptimized(  TimSortsOP( oneHalf ), TimSortsOP( otherHalf));		
+		}
+	}
+	private static Comparable[] mergeComOptimized(Comparable[] nums1, Comparable[] nums2) {
+		Comparable[] result = new Comparable[nums1.length+nums2.length];
+		int onePosition = 0;
+		int twoPosition = 0;
+		while(true)
+		{
+			if(onePosition > nums1.length-1) //when all of list1 is in the results array
+			{
+				for(int i = twoPosition; i < nums2.length; i++)
+				{
+					result[nums1.length+i] = nums2[i];
+				}
+				break;
+			}
+			if(twoPosition > nums2.length-1) //when all of list2 is in the results array
+			{
+				for(int i = onePosition; i < nums1.length; i++)
+				{
+					result[nums2.length+i] = nums1[i];
+				}
+				break;
+			}
+			if(  nums1[onePosition].compareTo(nums2[twoPosition]) < 0) // if list1 is lower
+			{
+				result[onePosition+twoPosition] = nums1[onePosition];
+				onePosition++;
+				continue;
+			}
+			else if(  nums1[onePosition].compareTo(nums2[twoPosition]) > 0) // if list2 is lower
+			{
+				result[onePosition+twoPosition] = nums2[twoPosition];
+				twoPosition++;
+				continue;
+			}
+			else
+			{
+				result[onePosition+twoPosition] = nums2[twoPosition];
+				twoPosition++;
+				continue;
+			}
+		}
+		return result;
 	}
 	public static void swapStringArray(String[] array, int i, int j) {
 		String temp = array[i];
